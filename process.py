fcd1d1342066ba245de37a6933c355d3ac98e69a
vagrant <vagrant@vagrant.vm>
2020-10-02 05:53:07 +0000

    Added Lines of comment about what code does

--- process.py
+++ process.py
@@ -2,12 +2,17 @@ log_file = open("um-server-01.txt")
 
 
 def sales_reports(log_file):
+#sets funtion with argument
     for line in log_file:
+    # iterates over log_file
         line = line.rstrip()
+        # breaks apart each line as a new data strip
         day = line[0:3]
+        # shows where the day is according to index
         if day == "Tue":
+        # creates conditional that acts if the day equals day requested
             print(line)
-
+        # prints out information so its visable in terminal
 
 
 sales_reports(log_file)
